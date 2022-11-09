a9ba365d1fdd4a3cc09466abe3daba64f7142c60
mblancov <mathys.blanco--volle@insa-cvl.fr>
2022-11-09 17:45:21 +0100

    add prefecture and prefect

--- Class.py
+++ Class.py
@@ -26,16 +26,21 @@ class Cell: #Une case de la map
         self.current_map = my_current_map
         self.type_of_cell = my_type_of_cell #type de la cellule (batiments = 2, nature/vide = 0, chemin = 1) : int
         self.nb_of_entity = nb_of_entity #le nombre d'entités présentes sur la case : int
+        self.water = False
+
 
     def inMap(self):
         return (0 <= self.x & self.x <= self.current_map.size-1 & 0 <= self.y & self.y <= self.current_map.size-1)
+
+    
+
             
 class Building(Cell) : #un fils de cellule (pas encore sûr de l'utilité)
     def __init__(self, x,y, my_current_map, my_type_of_building, my_state):
         super().__init__(x, y, my_current_map, 2)
         self.type_of_cell = 2
         self.type_of_building = my_type_of_building  #le type de batiments (house, fountain, ...) : ? 
-        self.state = my_state #état (détruit ou pas) : "underConstruction", "built", "destroyed" 
+        self.state = my_state #état (détruit ou pas) 
         self.timer = 0.00 #timer pour le feu : float
 
 
@@ -53,11 +58,11 @@ class Path(Cell):
 
 class House(Building) : #la maison fils de building (?)
     def __init__(self, x, y, my_current_map, level=0, nb_occupants=0) :
-        super().__init__(x, y, my_current_map, "house", "underConstruction")
+        super().__init__(x, y, my_current_map, "house", True)
         self.level = level #niveau de la maison : int
         self.nb_occupants = nb_occupants #nombre d'occupants: int
         self.max_occupants = 5 #nombre max d'occupant (dépend du niveau de la maison) : int
-        self.water = False
+        
 
     def check_fountain(self):
         for i in range(-2, 3):
@@ -66,10 +71,12 @@ class House(Building) : #la maison fils de building (?)
                 if cell.type_of_cell == 2:
                     if cell.type_of_building == "fountain":
                         self.water = True
+
+
         
 class Fountain(Building) :
     def __init__(self, x, y, my_current_map) : 
-        super().__init__(x, y, my_current_map, "fountain", "build")
+        super().__init__(x, y, my_current_map, "fountain", True)
         self.check_house()
     
     def check_house(self) : 
@@ -81,12 +88,24 @@ class Fountain(Building) :
                         cell.water = True
                 
 
-class Entity() : 
+class Prefecture(Building) :
+    def __init__(self, x, y, my_current_map):
+        super().__init__(x, y, my_current_map, "prefecture", True)
+        self.labor_advisor = LaborAdvisor(self.x, self.y, self.current_map.array[self.x][self.y])
+        self.employees = 0
+        self.prefect = Prefect(self.x, self.y, self.current_map.array[self.x][self.y], self)
+
+
+
+
+
+class Walker() : 
     def __init__(self, job, position_x, position_y, starting_Cell) :
         self.job = job #le métier (migrant, worker, etc) : int (?)
         self.position_x = position_x #position sur la map : int
         self.position_y = position_y #position sur la map : int
         self.current_Cell = starting_Cell #La cellule de départ de l'entity : Cell
+        self.previous_cell = None
 
     def move_up(self) : #bouger d'une case vers le haut
         assert (0 <= self.position_x < 40 and 0 <= self.position_y < 40)
@@ -109,6 +128,42 @@ class Entity() :
             self.current_Cell.value = self.current_Cell.type #On change la valeur de l'ancienne cellule (?) 
             self.current_Cell = new_cell
             #self.current_Cell.value = ?
+
+    def check_path(self) :
+        for i in range(-1, 1) :
+            for j in range(-1, 1) : 
+                if (abs(i) != abs(j) & self.current_prefecture.current_map.array[self.current_prefecture.x + i][self.current_prefecture.y + j].type_of_cell  == "path") : 
+                    self.cell_assignement(self.current_prefecture.current_map.array[self.current_prefecture.x + i][self.current_prefecture.y + j])
+
+
+
+class Prefect(Walker) : 
+    def __init__(self, position_x, position_y, starting_Cell, current_prefecture):
+        super().__init__("prefect" , position_x, position_y, starting_Cell)
+        self.prefect_in_building = True
+        self.current_prefecture = current_prefecture
+
+    def leave_prefecture(self) : 
+        if (self.current_prefecture.employees == 6) :
+            for i in range(-1, 1) :
+                for j in range(-1, 1) : 
+                    if (abs(i) != abs(j) & self.current_prefecture.current_map.array[self.current_prefecture.x + i][self.current_prefecture.y + j].type_of_cell  == "path") : 
+                        self.cell_assignement(self.current_prefecture.current_map.array[self.current_prefecture.x + i][self.current_prefecture.y + j])
+                        self.prefect_in_building = False 
+                        break
+
+
+    def prefect_move(self) :
+        
+
+
+
+class LaborAdvisor(Walker) : 
+    def __init__(self, position_x, position_y, starting_Cell):
+        super().__init__("labor advisor", position_x, position_y, starting_Cell)
+        
+
+
 
 
 
