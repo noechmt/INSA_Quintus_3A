9f8ba674654568d50c3eae06b9d35424eeaab631
thomas2mars <thomasdem2012@gmail.com>
2022-12-21 20:07:40 +0100

    SCRIPT IN MAIN TO CHECK THE PYTHON REQUIREMENTS

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
-import pygame
-
-from GUI.title_screen import title_screen
 from game_screen.game_screen import game_screen
+from GUI.title_screen import title_screen
+import pygame
+import sys
+import os
+
+# Check of the version of python
+assert sys.version[0:4] == "3.10", "Merci d'utiliser une version de python >= 3.10 !"
+
+# Check if all the dependencies are installed
+os.system(str(sys.executable) + " -m pip install -r requirements.txt")
 
 if __name__ == "__main__":
     if (title_screen()):
